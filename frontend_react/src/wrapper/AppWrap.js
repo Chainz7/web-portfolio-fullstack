78848efaf60c503730f742335f6da7123279e125
Chainz <61576690+Chainz7@users.noreply.github.com>
2023-01-25 22:51:13 +0700

    reactjs-work-done

--- frontend_react/src/wrapper/AppWrap.js
+++ frontend_react/src/wrapper/AppWrap.js
@@ -10,10 +10,10 @@ function AppWrap(Component, idName, classNames) {
           <div className='app__wrapper app__flex'>
             <Component />
 
-            <div className='copyright'>
+            {/* <div className='copyright'>
                 <p className='p-text'>@2023 RAFI</p>
                 <p className='p-text'>All rights reserved</p>
-            </div>
+            </div> */}
           </div>
           <NavigationDots active={idName} />
         </div>
