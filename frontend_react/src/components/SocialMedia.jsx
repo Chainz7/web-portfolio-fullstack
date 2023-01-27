6c64f393d9280464df295928770f30330f4258bf
Chainz <61576690+Chainz7@users.noreply.github.com>
2023-01-27 19:48:50 +0700

    Reactjs Done

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
-import { BsLinkedin, BsGithub, BsInstagram, BsYoutube } from 'react-icons/bs' 
+import { BsLinkedin, BsGithub, BsYoutube } from 'react-icons/bs' 
 
 function SocialMedia() {
   return (
     <div className='app__social'>
-        <div>
-            <BsLinkedin />
-        </div>
-        <div>
-            <BsGithub/>
-        </div>
-        <div>
-            <BsYoutube />
-        </div>
-        <div>
-            <BsInstagram />
-        </div>
+        <a href="https://www.linkedin.com/in/muhammad-rafi-ardiansyah" target='_blank'>
+            <div>
+                <BsLinkedin />
+            </div>
+        </a>
+        <a href="https://github.com/Chainz7" target='_blank'>
+            <div>
+                <BsGithub />
+            </div>
+        </a>
+        <a href="https://www.youtube.com/@chainz9492" target='_blank'>
+            <div>
+                <BsYoutube />
+            </div>
+        </a>
     </div>
   )
 }
